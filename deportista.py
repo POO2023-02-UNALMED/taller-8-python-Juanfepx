9b0f00a6bb76b1750178f56811d2e919e97b069b
Juanfepx <juanfepx@gmail.com>
2023-10-16 20:10:44 -0500

    clases deportista y persona

---
+++ deportista.py
@@ -0,0 +1,18 @@
+class Deportista():
+
+    def __init__(self, nombre, edad, altura, sexo, deporte, años):
+        super().__init__(nombre, edad, altura, sexo)
+        self._deporte = deporte
+        self._añosPracticando = años
+
+    def setAñosPracticando(self, años):
+        self._añosPracticando = años
+
+    def getAñosPracticando(self):
+        return self._añosPracticando
+
+    def setDeporte(self, deporte):
+        self._deporte = deporte
+
+    def getDeporte(self):
+        return self._deporte
